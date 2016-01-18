2f55b57d637506e373939800e7c9516485402b2a
Supriya <supriyar@qburst.com>
2016-01-18 17:09:06 +0530

    First commit

---
+++ header.js
@@ -0,0 +1,27 @@
+/**
+ * This provides header for all pages in Innovation Lab app.
+ *
+ * @providesModule Header
+ * @flow
+ */
+'use strict';
+import React, {
+  Component,
+  StyleSheet,
+  Text,
+  Image,
+  View
+} from 'react-native';
+
+class Header extends Component {
+  render () {
+    return (
+      <view>
+      <Text>dwdw ewewqe weqw  </Text>
+      </view>
+      );
+  }
+
+}
+
+module.exports = Header;
